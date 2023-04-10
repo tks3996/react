c427a7485b732cbc8121be74fe9fd1fa879bc019
tks3996 <stusharkanta@gmail.com>
2023-04-10 15:21:22 +0530

    react-app branch demo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        Hey , why are you worried????
+        These files are form react-app branch.
       </header>
     </div>
   );
